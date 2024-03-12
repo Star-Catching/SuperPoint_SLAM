7b0311cead813df11e0d223eb9a70ff44484ee7a
Star-Catching <1612627719@qq.com>
2024-03-12 21:23:41 +0800

    first commit

---
+++ build/CMakeFiles/SuerPoint_SLAM.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Converter.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Converter.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Frame.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Frame.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/FrameDrawer.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/FrameDrawer.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Initializer.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Initializer.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/KeyFrame.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/KeyFrame.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/KeyFrameDatabase.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/KeyFrameDatabase.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/LocalMapping.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/LocalMapping.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/LoopClosing.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/LoopClosing.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Map.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Map.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/MapDrawer.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/MapDrawer.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/MapPoint.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/MapPoint.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Optimizer.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Optimizer.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/PnPsolver.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/PnPsolver.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Random.cpp" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Random.cpp.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/SPextractor.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/SPextractor.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/SPmatcher.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/SPmatcher.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Sim3Solver.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Sim3Solver.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/SuperPoint.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/SuperPoint.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/System.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/System.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Timestamp.cpp" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Timestamp.cpp.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Tracking.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Tracking.cc.o"
+  "/home/wxz/SLAM_code/SuperPoint_SLAM/src/Viewer.cc" "/home/wxz/SLAM_code/SuperPoint_SLAM/build/CMakeFiles/SuerPoint_SLAM.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "SuerPoint_SLAM_EXPORTS"
+  "USE_C10D_GLOO"
+  "USE_C10D_MPI"
+  "USE_DISTRIBUTED"
+  "USE_RPC"
+  "USE_TENSORPIPE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/usr/include/eigen3"
+  "/home/wxz/3dparty/pytorch/torch/include"
+  "/home/wxz/3dparty/pytorch/torch/include/torch/csrc/api/include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
